647cc5b88f1c1c985a278e2d750a3db29acead82
Devs Arun <itschool408@gmail.com>
2026-05-08 16:24:34 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,31 +1,27 @@
 let loggedIn = false;
 
 function handleCredentialResponse(response) {
-
     const data = parseJwt(response.credential);
-
     localStorage.setItem("userEmail", data.email);
     loggedIn = true;
-
     alert("Welcome " + data.name);
 }
 
 function parseJwt(token) {
-var base64Url = token.split('.')[1];
-var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-}).join(''));
-
-return JSON.parse(jsonPayload);
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
 }
 
-function checkLogin(){
-
-if(!localStorage.getItem("userEmail")){
-alert("Login with Google first");
-document.getElementById("loginBtn").click();
-return false;
-}
-
+function checkLogin() {
+    // TODO: Enable login check when auth is implemented
+    // if (!localStorage.getItem("userEmail")) {
+    //     alert("Login with Google first");
+    //     document.getElementById("loginBtn").click();
+    //     return false;
+    // }
+    return true;
 }
